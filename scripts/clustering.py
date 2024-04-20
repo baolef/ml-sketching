70ed99183cfb1dba15bfa0f25c890ee09ca19f41
Baole Fang <baole.fang@gmail.com>
2024-04-20 15:47:57 -0400

    feat: clustering

--- scripts/clustering.py
+++ scripts/clustering.py
@@ -1,16 +1,13 @@
 # Created by Baole Fang at 4/18/24
 
 import argparse
-import os
-
 import numpy as np
 from sklearn.cluster import AgglomerativeClustering
 from sklearn.metrics.pairwise import pairwise_distances
 from sklearn.metrics import adjusted_rand_score as ari
-from imblearn.under_sampling import RandomUnderSampler
-from collections import Counter
 
-def load(path, idx: int = 0, n=50):
+
+def load(path, idx: int = 0, n=10000):
     data = np.load(path, allow_pickle=True)
     X = []
     Y = []
@@ -29,7 +26,10 @@ def load(path, idx: int = 0, n=50):
     #         Y_.append(y)
     # X, Y = np.array(X_), np.array(Y_)
     # X, Y = sampler.fit_resample(X, Y)
-    return np.array(X)[:50000], np.array(Y)[:50000]
+    # return X, Y
+    np.random.seed(args.seed)
+    idx = np.random.choice(len(Y), n, replace=False)
+    return np.array(X)[idx], np.array(Y)[idx]
 
 
 def jaccard_distance(x, y):
@@ -40,18 +40,19 @@ def jaccard_distance(x, y):
 
 def main(args):
     print("Loading data...")
-    X, y = load(args.input, args.column)
+    X, y = load(args.input, args.column, args.n)
     n_classes = len(set(y))
     print(f'Number of samples: {len(y)}')
     print(f'Number of classes: {n_classes}')
-    distance=pairwise_distances(X, metric=args.distance, n_jobs=-1)
+    distance = pairwise_distances(X, metric=args.distance, n_jobs=-1)
     print(f'Clustering with {args.distance} distance...')
     model = AgglomerativeClustering(n_clusters=n_classes, metric='precomputed', linkage='average')
     model.fit(distance)
     score = ari(y, model.labels_)
     print(f'Adjusted Rand Index: {score}')
     with open(args.output, 'a') as f:
-        f.write(f'input: {args.input}, distance: {args.distance}, level: {args.column}, n_classes: {n_classes}, ari: {score}\n')
+        f.write(
+            f'input: {args.input}, distance: {args.distance}, level: {args.column}, n_samples: {len(y)}, n_classes: {n_classes}, ari: {score}\n')
 
 
 if __name__ == '__main__':
@@ -61,8 +62,7 @@ if __name__ == '__main__':
     parser.add_argument('-c', '--column', help='column/index of label', type=int, default=0)
     parser.add_argument('-t', '--test', help='test set size', type=float, default=0.2)
     parser.add_argument('-o', '--output', help='path of output model', type=str, default='outputs/clustering.txt')
-    parser.add_argument('-m', '--model', help='model name', type=str, default='xgboost')
+    parser.add_argument('-n', '--n', help='number of samples', type=int, default=10000)
     parser.add_argument('-s', '--seed', help='random seed', type=int, default=0)
     args = parser.parse_args()
     main(args)
-
