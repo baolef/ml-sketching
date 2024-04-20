5e5fb7e2fd3dd449b5253a9447ec4ed3f4da92de
Baole Fang <baole.fang@gmail.com>
2024-04-20 01:29:09 -0400

    fix: minhash to fixed dimension

--- scripts/sketch_minhash.py
+++ scripts/sketch_minhash.py
@@ -10,72 +10,20 @@ from rna2vec.generators import DisjointKmerFragmenter, SlidingKmerFragmenter, Se
 from tqdm import tqdm
 
 import multiprocessing
+from datasketch import MinHash
 
 
 class InvalidArgException(Exception):
     pass
 
 
-def rand_prime():
-    """Returns a random prime number
-    Input:
-        None
-    Returns: 1 items as so:
-    a random prime number
-    """
-    while True:
-        p = np.random.randint(2 ** 32 - 1)
-        if all(p % i != 0 for i in range(2, int(np.sqrt(p)) + 1)):
-            return p
-
-
-def universal_hashing():
-    """Returns a hash function from a family of universal hash functions
-    Input:
-        None
-    Returns: 1 items as so:
-    a hash function
-    """
-    m = 2 ** 32 - 1
-    p = rand_prime()
-    a = np.random.randint(0, p)
-    if a % 2 == 0:
-        a += 1
-    b = np.random.randint(0, p)
-
-    def h(x):
-        x = hash(x)
-        return ((a * x + b) % p) % m
-
-    return h
-
-def kmerize(seq, k):
-    """Returns a list of all k-mers in seq
-    Input:
-        seq: string
-        k: int
-    Returns: 1 items as so:
-    a list of all k-mers in seq
-    """
-    return [seq[i:i + k] for i in range(len(seq) - k + 1)]
-
-
-def minhash_kmer(kmer, hash_functions):
-    """Returns the minimum hash value of kmer using hash_functions
-    Input:
-        kmer: string
-        hash_functions: list of hash functions
-    Returns: 1 items as so:
-    the minimum hash value of kmer using hash_functions
-    """
-    return min([h(kmer) for h in hash_functions])
-
-
 def minhash(seq):
+    m=MinHash(num_perm=args.vec_dim)
     seq = ''.join(splitter.get_acgt_seqs(str(seq)))
-    fragments = kmerize(seq, fragmenter.k_high)
-    hash = [minhash_kmer(fragment, hash_functions) for fragment in fragments]
-    return hash
+    fragments = fragmenter.apply(rng, seq)
+    for fragment in fragments:
+        m.update(fragment.encode('utf-8'))
+    return m.hashvalues
 
 
 def main():
@@ -88,7 +36,8 @@ def main():
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='convert dna dataset to vectors')
-    parser.add_argument('-v', '--vec-dim', help='vector dimension', type=int, default=12)
+    parser.add_argument('-v', '--vec-dim', help='vector dimension', type=int, default=100)
+    parser.add_argument('--hash', help='hash functions', type=int, default=100)
     parser.add_argument('-l', '--low', help='lower bound of k', type=int, default=3)
     parser.add_argument('-u', '--up', help='upper bound of k', type=int, default=8)
     parser.add_argument('-i', '--input', help='path to the input dataset', type=str, required=True)
@@ -107,7 +56,6 @@ if __name__ == '__main__':
         raise InvalidArgException('Invalid kmer fragmenter: {}'.format(args.kmer_fragmenter))
 
     splitter = SeqFragmenter()
-    hash_functions = [universal_hashing() for _ in range(args.vec_dim)]
 
     records = list(SeqIO.parse(args.input, args.type))
     rng = np.random.RandomState(args.seed)
